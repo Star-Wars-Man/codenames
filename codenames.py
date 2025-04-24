1f0dcfbaedac4f6874d1130f89d3247f58665a26
Star-Wars-Man <jacob@olsenweb.net>
2025-04-24 10:16:39 -0600

    first

---
+++ codenames.py
@@ -0,0 +1,21 @@
+def main():
+    createBoards()
+    setPlayers()
+    gameLoop()
+    displayWinner()
+
+def createBoards():
+    word_board = []
+    
+    
+def setPlayers():
+    
+
+def checkIfGameOver():
+    
+
+def gameLoop():
+    checkIfGameOver()
+
+def displayWinner():
+    
