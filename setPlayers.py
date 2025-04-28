545afb245274697cede6b52296649c4e23169473
Joseph Weaver <26weavejos@washk12.org>
2025-04-28 09:10:01 -0600

    classwork

--- setPlayers.py
+++ setPlayers.py
@@ -1,5 +1,11 @@
 def setPlayers():
-    userQuestion = input("Type number of players")
-    return str(userQuestion)
-    
-print(setPlayers())
+    print('Players 1 and 3 are Red Team, Players 2 and 4 are Blue Team')
+    print('Players 1 and 2 are Clue Givers, Players 3 and 4 are Guessers')
+
+redTeamClueGiver = 1
+
+blueTeamClueGiver = 3
+
+redTeamGuesser = 2
+
+blueTeamGuesser = 4
