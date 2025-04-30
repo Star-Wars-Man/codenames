10317be16e481dceeb08a83f984a0e9ed4f8551a
Star-Wars-Man <jacob@olsenweb.net>
2025-04-30 08:43:53 -0600

    setPlayers firstPlayer

--- codenames.py
+++ codenames.py
@@ -24,8 +24,20 @@ def createBoards():
     
     return [chosen_matrix, matrix]
     
-def setPlayers():
-    print("hi")
+def setPlayers(color_board):
+    print('Players 1 and 3 are Red Team, Players 2 and 4 are Blue Team')
+    print('Players 1 and 2 are Clue Givers, Players 3 and 4 are Guessers')
+    
+    r_count = 0
+    for row in color_board:
+        for color in row:
+            if color == "red":
+                r_count += 1
+    
+    if r_count == 9:
+        return "red"
+    else:
+        return "blue"
 
 def checkIfGameOver(color_board, chosen_board, turn):
     # Check if the black tile was chosen
--- setPlayers.py
+++ setPlayers.py
@@ -1,11 +1,14 @@
-def setPlayers():
+def setPlayers(color_board):
     print('Players 1 and 3 are Red Team, Players 2 and 4 are Blue Team')
     print('Players 1 and 2 are Clue Givers, Players 3 and 4 are Guessers')
-
-redTeamClueGiver = 1
-
-blueTeamClueGiver = 3
-
-redTeamGuesser = 2
-
-blueTeamGuesser = 4
+    
+    r_count = 0
+    for row in color_board:
+        for color in row:
+            if color == "red":
+                r_count += 1
+    
+    if r_count == 9:
+        return "red"
+    else:
+        return "blue"
