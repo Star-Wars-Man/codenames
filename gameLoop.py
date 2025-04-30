d115877369c01a6aae26143497c5527764460a5f
Joseph Weaver <26weavejos@washk12.org>
2025-04-30 08:31:32 -0600

    classwork

--- gameLoop.py
+++ gameLoop.py
@@ -1,6 +1,12 @@
 def gameLoop(color_board, word_board, chosen_board):
-    
+
+playerAction = {
+    'Player 1': 'give clue',
+    'Player 3': 'take guesses',
+    'Player 2': 'give clue',
+    'Player 4': 'take guesses'
+}
 
 
 
-    checkIfGameOver(color_board, chosen_board, turn)
+checkIfGameOver(color_board, chosen_board, turn)
