f89c50b044c21c6d80d4c1bb87bd8cce4ce73869
Joseph Weaver <26weavejos@washk12.org>
2025-04-30 09:20:25 -0600

    classwork

--- gameLoop.py
+++ gameLoop.py
@@ -8,5 +8,32 @@ playerAction = {
 }
 
 
+def gameLoop(color_board, word_board, chosen_board, firstPlayer):
+    print(firstPlayer + " Team is going first, guessers leave")
+    displayBoards(color_board, word_board, 'c')
+    clue = input('enter one word clue')
+    numOfClues = input('number applying to it: ')
+    print(firstPlayer + ' Guessers turn')
+    displayBoards(color_board, word_board, 'g')
+    for i in range(numOfClues):
+        clueR = input('enter row of guess (starts at 0 ends at 4)')
+        clueC = input('enter column of guess (starts at 0 ends at 4)')
+        if color_board[clueR][clueC] == firstPlayer:
+            chosen_board[clueR][clueC] = 'y'
+        else:
+            print('Wrong guess')
+            break
+    over = checkIfGameOver(color_board, chosen_board, firstPlayer)
+
+    
+        
+    print(displayBoards)
+    input()
+
+        
+
+    
+    checkIfGameOver(color_board, chosen_board, turn)
+
 
 checkIfGameOver(color_board, chosen_board, turn)
